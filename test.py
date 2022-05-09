5a7912ae1f411d8247a91327f9ab19ce99328192
WoobinHwang <95206741+WoobinHwang@users.noreply.github.com>
2022-05-09 22:57:38 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+# test = divmod(30, 30)
+# print(test)
+
+# # (1, 0)
+
+# for i in range(2):
+#     print(i)
+# # 0 , 1
+
+a= 0
+while True:
+    print("good", a)
+    a = a + 1
+    if a == 3:
+        break
